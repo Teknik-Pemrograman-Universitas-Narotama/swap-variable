b29fbbaf0979b02762210fea237f5135f54ee8fb
Noor Azam <noor@rad.net.id>
2020-02-17 11:32:40 +0700

    release v.1

---
+++ swap-variable.py
@@ -0,0 +1,12 @@
+A = input("Masukkan angka #1 : ")
+B = input("Masukkan angka #2 : ")
+
+print("Anda memasukkan bilangan A = ", A)
+print("dan bilangan B = ", B)
+
+C = A
+A = B
+B = C
+
+print("Setelah swap, bilangan A = ", A)
+print("dan bilangan B = ", B)
